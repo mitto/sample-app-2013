bfa15c2db3786f3998e94705ee092bf9b4cd3009
mitto nagisa <mitto@mittostar.info>
2013-11-27 11:29:18 +0900

    implement user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true,
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+
+  has_secure_password
 end
