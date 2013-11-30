c9ba47f18c22e0dbc72a43f1a210f936269c62a4
mitto nagisa <mitto@mittostar.info>
2013-11-30 18:21:15 +0900

    add visiting the users index page spec

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -45,6 +45,11 @@ describe "AuthenticationPages" do
 
       describe "in the Users controller" do
 
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
           it { should have_title('Sign in') }
